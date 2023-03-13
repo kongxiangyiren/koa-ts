dd49e89a2b6f5d9910313430967d53363c67e305
空巷一人 <1530688385@qq.com>
2023-03-13 17:00:08 +0800

    vercel-router

---
+++ src/config/route.vercel.ts
@@ -0,0 +1,14 @@
+
+  import { type DefaultState, type DefaultContext } from 'koa';
+  import router0 from '../routes/api/index';
+import router1 from '../routes/index';
+
+
+  export default async function router(app: import('koa')<DefaultState, DefaultContext>) {
+ app.use(router0.routes())
+app.use(router1.routes())
+
+    
+  }
+  
+  
